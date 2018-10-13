c1a61c0633d938debc5d04392a398afeebb4d055
yongkyu.lee <fomuon@gmail.com>
2018-10-13 22:31:15 +0900

    Initial commit

---
+++ src/main/java/org/example/bootiful/security/BootifulSecurityExampleApplication.java
@@ -0,0 +1,12 @@
+package org.example.bootiful.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulSecurityExampleApplication.class, args);
+	}
+}
